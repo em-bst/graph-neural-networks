5ebf3c6645a43be1431eb90d1ede24b92d091ed4
TheoBacqueyrisse <144768551+TheoBacqueyrisse@users.noreply.github.com>
2023-12-25 08:51:13 +0000

    upd utils

--- utils.py
+++ utils.py
@@ -15,7 +15,7 @@ from torch_geometric.loader import DataLoader
 # Neural Network Architecture
 from torch.nn import Linear
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv, GATConv, GINConv
+from torch_geometric.nn import GCNConv, GATConv, GINConv, TransformerConv
 from torch_geometric.nn import global_max_pool as gmp, global_mean_pool as gap, global_add_pool as gad
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
@@ -24,6 +24,3 @@ from torch.nn import MSELoss, L1Loss
 
 # Optimizer
 from torch.optim import Adam, SGD, Adagrad
-
-# See the progression of the Training
-import tqdm
